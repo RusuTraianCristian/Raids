a3d7f369adf4552a3da30e248c27330db4a29849
Rusu Traian Cristian <RusuTraianCristian@users.noreply.github.com>
2019-04-02 15:27:40 +0300

    Add files via upload

--- src/components/Products.js
+++ src/components/Products.js
@@ -25,6 +25,23 @@ class Products extends React.Component {
     buyRaid(e) {
         window.Twitch.ext.bits.getProducts().then(products => {
             Twitch.ext.bits.useBits(`raid${e}`);
+            window.Twitch.ext.bits.onTransactionComplete(TransactionObject => {
+                const setBitsRaised = TransactionObject.product.cost.amount;
+                // POST
+                window.Twitch.ext.onAuthorized(async auth => {
+                    fetch('https://fng6b6xn2c.execute-api.us-east-1.amazonaws.com/firstStage/bitsraised', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            id: auth.channelId,
+                            setBitsRaised: setBitsRaised
+                        })
+                    });
+                }); // END of POST
+            });
         });
     }
 
