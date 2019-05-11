de81177b92caaa1325eb86653fa12391c6194c23
RusuTraianCristian <dad17_cc@yahoo.com>
2019-05-11 15:26:58 +0300

    dasda

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,7 +4,8 @@ class Products extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            options: []
+            options: [],
+            auth: {}
         };
     }
 
@@ -20,47 +21,49 @@ class Products extends React.Component {
                 options: [...newArr]
             });
         });
+        window.Twitch.ext.onAuthorized(auth => {
+            this.setState({
+                auth: {...auth}
+            });
+        });
     }
 
     buyRaid(e) {
+        const bitsRaisedUrl = 'https://fng6b6xn2c.execute-api.us-east-1.amazonaws.com/firstStage/bitsraised';
+
         window.Twitch.ext.bits.getProducts().then(products => {
             Twitch.ext.bits.useBits(`raid${e}`);
             // START of POST
             window.Twitch.ext.bits.onTransactionComplete(TransactionObject => {
                 const setBitsRaised = TransactionObject.product.cost.amount;
-                const bitsRaisedUrl = 'https://fng6b6xn2c.execute-api.us-east-1.amazonaws.com/firstStage/bitsraised';
-                window.Twitch.ext.onAuthorized(async auth => {
-                    fetch(bitsRaisedUrl, {
-                        method: 'POST',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            id: auth.channelId,
-                            setBitsRaised: setBitsRaised
-                        })
-                    }).then(response => {this.sendExtensionChatMessage(setBitsRaised)});
-                });
+                fetch(bitsRaisedUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        id: this.state.auth.channelId,
+                        setBitsRaised: setBitsRaised
+                    })
+                }).then(response => {this.sendExtensionChatMessage(setBitsRaised)});
             });
             // END of POST
         });
     }
 
     sendExtensionChatMessage = (eb) => {
-        window.Twitch.ext.onAuthorized(auth => {
-            fetch(`https://api.twitch.tv/extensions/${auth.clientId}/0.0.1/channels/${auth.channelId}/chat`, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + auth.token,
-                    'Client-ID': auth.clientId
-                },
-                body: JSON.stringify({
-                    'text': 'A member supported the raid with ' + eb + ' bits.'
-                })
-            });
+        fetch(`https://api.twitch.tv/extensions/${this.state.auth.clientId}/0.0.1/channels/${this.state.auth.channelId}/chat`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.state.auth.token,
+                'Client-ID': this.state.auth.clientId
+            },
+            body: JSON.stringify({
+                'text': `${this.state.auth.userId} supported the raid with ${eb} bits.`
+            })
         });
     }
 
